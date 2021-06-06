79ebe44b546dd89bfb6bf1dea3306968df2e4e70
Yuma Gunawan <amazingyuma0@gmail.com>
2021-06-07 05:16:22 +0800

    fixing test scenario

--- app/src/androidTest/java/com/u1/movieapp/ui/home/MainActivityTest.kt
+++ app/src/androidTest/java/com/u1/movieapp/ui/home/MainActivityTest.kt
@@ -24,7 +24,7 @@ class MainActivityTest {
     var activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun onFragmentMovies() {
+    fun loadMovies() {
         //click fragment
         onView(withId(R.id.navigation_movies)).perform(click())
         //check fragment display
@@ -35,17 +35,10 @@ class MainActivityTest {
         //check rv poster desc display
         onView(withId(R.id.rv_poster_desc_movies)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_poster_desc_movies)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dataMoviesPopular.size))
-
-        //click on rv poster movies
-        onView(withId(R.id.rv_poster_movies)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        //test only one attribute because its parcelable should be the same to another
-        onView(withId(R.id.title)).check(matches(isDisplayed()))
-        onView(withId(R.id.title)).check(matches(withText(dataMoviesPlaying[0].title)))
     }
 
-
     @Test
-    fun onFragmentSeries() {
+    fun loadSeries() {
         //click fragment series
         onView(withId(R.id.navigation_series)).perform(click())
         //check fragment display
@@ -56,11 +49,53 @@ class MainActivityTest {
         //check rv poster desc display
         onView(withId(R.id.rv_poster_desc_series)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_poster_desc_series)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dataSeriesPopular.size))
+    }
 
+    @Test
+    fun loadMoviesDetail() {
+        //click fragment
+        onView(withId(R.id.navigation_movies)).perform(click())
+        //check fragment display
+        onView(withId(R.id.fragment_movies)).check(matches(isDisplayed()))
+        //check rv poster display
+        onView(withId(R.id.rv_poster_movies)).check(matches(isDisplayed()))
+        //click on rv poster movies
+        onView(withId(R.id.rv_poster_movies)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        onView(withId(R.id.title)).check(matches(isDisplayed()))
+        onView(withId(R.id.title)).check(matches(withText(dataMoviesPlaying[0].title)))
+
+        onView(withId(R.id.genre)).check(matches(isDisplayed()))
+        onView(withId(R.id.genre)).check(matches(withText(dataMoviesPlaying[0].genre)))
+
+        onView(withId(R.id.date)).check(matches(isDisplayed()))
+        onView(withId(R.id.date)).check(matches(withText(dataMoviesPlaying[0].date)))
+
+        onView(withId(R.id.duration)).check(matches(isDisplayed()))
+        onView(withId(R.id.duration)).check(matches(withText(dataMoviesPlaying[0].duration)))
+    }
+
+    @Test
+    fun loadSeriesDetail() {
+        //click fragment series
+        onView(withId(R.id.navigation_series)).perform(click())
+        //check fragment display
+        onView(withId(R.id.fragment_series)).check(matches(isDisplayed()))
+        //check rv poster display
+        onView(withId(R.id.rv_poster_series)).check(matches(isDisplayed()))
         //click on rv poster series
         onView(withId(R.id.rv_poster_series)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
-        //test only one attribute because its parcelable should be the same to another
+
         onView(withId(R.id.title)).check(matches(isDisplayed()))
         onView(withId(R.id.title)).check(matches(withText(dataSeriesPlaying[0].title)))
+
+        onView(withId(R.id.genre)).check(matches(isDisplayed()))
+        onView(withId(R.id.genre)).check(matches(withText(dataSeriesPlaying[0].genre)))
+
+        onView(withId(R.id.date)).check(matches(isDisplayed()))
+        onView(withId(R.id.date)).check(matches(withText(dataSeriesPlaying[0].date)))
+
+        onView(withId(R.id.duration)).check(matches(isDisplayed()))
+        onView(withId(R.id.duration)).check(matches(withText(dataSeriesPlaying[0].duration)))
     }
 }
--- app/src/test/java/com/u1/movieapp/ui/detail/DetailActivityTest.kt
+++ app/src/test/java/com/u1/movieapp/ui/detail/DetailActivityTest.kt
@@ -3,6 +3,7 @@ package com.u1.movieapp.ui.detail
 import com.u1.movieapp.data.model.DummyData
 import com.u1.movieapp.data.utils.DummyBuilder
 import com.u1.movieapp.ui.home.content.movies.MoviesViewModel
+import com.u1.movieapp.ui.home.content.series.SeriesViewModel
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -10,28 +11,52 @@ import org.junit.Before
 
 class DetailActivityTest {
 
-    private lateinit var viewModel : MoviesViewModel
+    private lateinit var moviesViewModel : MoviesViewModel
+    private lateinit var seriesViewModel : SeriesViewModel
     private lateinit var dataFromFragment: DummyData
 
     @Before
     fun setUp(){
-        viewModel = MoviesViewModel()
-        dataFromFragment = viewModel.getMoviesPopular()[0]
+        moviesViewModel = MoviesViewModel()
+        seriesViewModel = SeriesViewModel()
+
     }
 
     @Test
-    fun setView() {
+    fun setViewMovies() {
+        dataFromFragment = moviesViewModel.getMoviesPopular()[0]
 
         //testing only one data from movies because all of them have same mechanism
         val dummyData = DummyBuilder.generateDummyMoviesPopular()[0]
 
+        //check data from view model not null
         assertNotNull(dataFromFragment)
+
+        //check data same as expected
         assertEquals(dummyData.posterImg, dataFromFragment.posterImg)
         assertEquals(dummyData.title, dataFromFragment.title)
         assertEquals(dummyData.genre, dataFromFragment.genre)
         assertEquals(dummyData.duration, dataFromFragment.duration)
         assertEquals(dummyData.date, dataFromFragment.date)
         assertEquals(dummyData.desc, dataFromFragment.desc)
+    }
 
+    @Test
+    fun setViewSeries() {
+        dataFromFragment = seriesViewModel.getSeriesPopular()[0]
+
+        //testing only one data from movies because all of them have same mechanism
+        val dummyData = DummyBuilder.generateDummySeriesPopular()[0]
+
+        //check data from view model not null
+        assertNotNull(dataFromFragment)
+
+        //check data same as expected
+        assertEquals(dummyData.posterImg, dataFromFragment.posterImg)
+        assertEquals(dummyData.title, dataFromFragment.title)
+        assertEquals(dummyData.genre, dataFromFragment.genre)
+        assertEquals(dummyData.duration, dataFromFragment.duration)
+        assertEquals(dummyData.date, dataFromFragment.date)
+        assertEquals(dummyData.desc, dataFromFragment.desc)
     }
 }
